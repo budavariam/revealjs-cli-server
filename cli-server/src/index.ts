8664cf374177ab50a59666c297fca236e15c1601
Mátyás Budavári <budavariam@gmail.com>
2020-12-15 09:33:06 +0100

    Add tests, package json missing parts

--- cli-server/src/index.ts
+++ cli-server/src/index.ts
@@ -1,11 +1,12 @@
+import * as fs from 'fs'
+import * as jetpack from "fs-jetpack";
+import * as matter from 'gray-matter'
+import * as path from 'path'
+
+import { defaultConfiguration, IDocumentOptions } from "./Configuration"
 import { Logger, LogLevel } from "./Logger"
 import { RevealServer } from "./RevealServer"
-import { defaultConfiguration, IDocumentOptions } from "./Configuration"
 import { parseSlides } from "./SlideParser"
-import * as jetpack from "fs-jetpack";
-import * as path from 'path'
-import * as matter from 'gray-matter'
-import * as fs from 'fs'
 
 const rootDir = '.'
 
@@ -48,8 +49,8 @@ async function main(slideSource, overrideConfig = {}) {
     const documentText = "" + fs.readFileSync(slideSource)
     await jetpack.removeAsync(getExportPath(config))
     const server = new RevealServer(
-        new Logger(LogLevel.Verbose, () => { }),
-        () => rootDir, //RootDir
+        new Logger(LogLevel.Verbose, (e) => { console.log(e) }),
+        () => rootDir, // RootDir
         () => parseSlides(documentText, documentOptions(documentText)),
         () => config,
         '.', // PATH TO DATA
@@ -75,7 +76,7 @@ async function main(slideSource, overrideConfig = {}) {
     console.log(`Use this url to export pdf: ${exportPDFUri(server)}`)
     console.log(`Speaker view served from: ${getUri(server)}libs/reveal.js/3.8.0/plugin/notes/notes.html (NEEDS TO OPEN WITH SHORTCUT)`)
 
-    //server.stop()
+    // server.stop()
 
 }
 
